bfa1df54bd9a8481c5eafea1be0bc98df71651c5
Lucas Gabriel <90lucasgabriel@gmail.com>
2019-10-02 20:04:39 -0300

    - Fix: Yellow warnings;

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {Provider} from 'react-redux';
 import Layout from './components/layout';
 import RootNavigator from './navigation/navigators/index';
 import store from './store';
+console.disableYellowBox = true;
 
 export default class App extends Component {
   render() {
