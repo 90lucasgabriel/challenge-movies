1b0e71853a68674501c28e6bd51f4b3205e60b29
Lucas Gabriel <90lucasgabriel@gmail.com>
2019-10-02 21:34:26 -0300

    - Update: Adjust gitignore and dependecies;

--- src/store.js
+++ src/store.js
@@ -1,11 +1,9 @@
 import {applyMiddleware, createStore, compose} from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
-import {logger} from 'redux-logger';
-
 import rootReducer from './reducers';
 
-const middleware = applyMiddleware(promiseMiddleware, thunk, logger);
+const middleware = applyMiddleware(promiseMiddleware, thunk);
 const tilteStore = createStore(rootReducer, middleware);
 
 export default tilteStore;
